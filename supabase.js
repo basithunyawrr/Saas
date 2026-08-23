7fd80fba6a5b8a7ffa6cf5a2d3eab20d3d7d08bf
basithunyawrr <basithadi@gmail.com>
2026-08-23 23:17:37 +0500

    build

--- supabase.js
+++ supabase.js
@@ -1,7 +1,9 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 const SUPABASE_URL = "https://fchwqspjwpxqksdwglfb.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "YOUR_PUBLISHABsb_publishable_L_ZIs7dVwBBrQkmU7kfzbg_0QR-ZnwTLE_KEY";
+
+const SUPABASE_PUBLISHABLE_KEY =
+    "sb_publishable_L_ZIs7dVwBBrQkmU7kfzbg_0QR-ZnwT";
 
 export const supabase = createClient(
     SUPABASE_URL,
